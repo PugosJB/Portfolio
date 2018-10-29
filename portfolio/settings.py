1aa1742933c45aec3b93cfb194fd1a621e9dabd3
Unknown <jonas.bruemmer@web.de>
2018-10-29 19:57:46 +0100

    Wieder Rückgängig

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_mysql',
 
     ]
 
@@ -78,19 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DB3544774',
-        'USER': 'U3544774',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
         'PASSWORD':'PiePer2010',
-        'HOST':'rdbms.strato.de',
-        #'PORT':'',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            # Tell MySQLdb to connect with 'utf8mb4' character set
-            'charset': 'utf8mb4',
-        },
-
-
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
